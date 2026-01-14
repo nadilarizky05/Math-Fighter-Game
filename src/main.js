318fbfd62227f5871c38be65ff698d1f667eb83a
Nadila Rizky <nadilarizkyamelia5@gmail.com>
2026-01-14 11:45:37 +0800

    first commit

---
+++ src/main.js
@@ -0,0 +1,26 @@
+import Phaser from 'phaser'
+
+import MathFighterScene from './scene/MathFigtherScene'
+
+
+const config = {
+	type: Phaser.AUTO,
+	parent: 'app',
+	width: 480,
+	height: 640,
+	physics: {
+		default: 'arcade',
+		arcade: {
+			gravity: { y: 200 },
+			debug: true
+		},
+	},
+	scene: [MathFighterScene],
+	scale: {
+		mode: Phaser.Scale.FIT,
+		autoCenter: Phaser.Scale.CENTER_BOTH
+	},
+}
+
+export default new Phaser.Game(config)
+
